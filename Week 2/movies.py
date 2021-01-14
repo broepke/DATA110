dd83524cc34cd2ca12256b664340589f19f81d91
Brian Roepke <broepke@gmail.com>
2021-01-14 09:47:12 -0800

    Update movies.py

--- Week 2/movies.py
+++ Week 2/movies.py
@@ -2,70 +2,70 @@
 dictionary with movie genres from imdb
 each genre is a list of dictionary objects
 '''
-moviesDict = { "movies": {
-  # horror titles
+moviesDict = {"movies": {
+    # horror titles
     "horror": [
-      { "title": "The Lodge",
-        "year": 2019,
-        "rating": 6.1,
-      },
-      { "title": "The Platform",
-        "year": 2019,
-        "rating": 7.0,
-      },
+        {"title": "The Lodge",
+         "year": 2019,
+         "rating": 6.1,
+         },
+        {"title": "The Platform",
+            "year": 2019,
+            "rating": 7.0,
+         },
     ],
     # comedy titles
     "comedy": [
-      { "title": "Trolls World Tour",
-        "year": 2020,
-        "rating": 6.1
-      }
+        {"title": "Trolls World Tour",
+         "year": 2020,
+         "rating": 6.1
+         }
     ],
-  }
+}
 }
 
 # returns all horror movies
-horror = moviesDict["movies"].get("horror") 
+horror = moviesDict["movies"].get("horror")
 
 # add a new horror movie
 # first create a dictionary
-newTitle = { "title": "Midsommar",
-        "year": 2019,
-        "rating": 7.1,
-      }
+newTitle = {"title": "Midsommar",
+            "year": 2019,
+            "rating": 7.1,
+            }
 
 # add the new title to horror. Note the use of append for lists
 horror.append(newTitle)
 
 # add another title, notice shorter code.
-horror.append({ "title": "The Invisible Man",
-        "year": 2020,
-        "rating": 7.2,
-      })
+horror.append({"title": "The Invisible Man",
+               "year": 2020,
+               "rating": 7.2,
+               })
 
 # add animation genre to the movies dictionary
 animations = [
-      { "title": "The Willoughbys",
-        "year": 2020,
+    {"title": "The Willoughbys",
+     "year": 2020,
         "rating": 6.4,
-      },
-       { "title": "Trolls World Tour",
+     },
+    {"title": "Trolls World Tour",
         "year": 2020,
-        "rating": 6.1,
-      },
-       { "title": "Onward (I)",
+     "rating": 6.1,
+     },
+    {"title": "Onward (I)",
         "year": 2020,
-        "rating": 7.5,
-      },
-       { "title": "The Lion King",
+     "rating": 7.5,
+     },
+    {"title": "The Lion King",
         "year": 2019,
-        "rating": 6.9,
-      },
-       { "title": " Frozen II",
+     "rating": 6.9,
+     },
+    {"title": " Frozen II",
         "year": 2019,
-        "rating": 7.0,
-      },
-    ]
+     "rating": 7.0,
+     },
+]
 moviesDict['movies']['animation'] = animations
 
 # add more genres with movie titles
@@ -73,10 +73,8 @@ moviesDict['movies']['animation'] = animations
 # display details of each genre
 genres = moviesDict['movies']
 for gen in genres:
-  print(gen)
-  movies = moviesDict['movies'][gen]
-  
+    print(gen)
+    movies = moviesDict['movies'][gen]
 
-  # add remaining code to get the average rating movies by genre.
-  # hint: use inner for-loop to loop through each movie of movies
-   
+    # add remaining code to get the average rating movies by genre.
+    # hint: use inner for-loop to loop through each movie of movies
