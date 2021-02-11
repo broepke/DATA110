de52cc8ee678d4e5de7af2ef6817efb8e1901fee
Brian Roepke <broepke@gmail.com>
2021-02-11 06:22:05 -0800

    Random

--- Testing/spyder.py
+++ Testing/spyder.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def fib(n):
     fibs = []
     a, b = 0, 1
@@ -9,6 +11,28 @@ def fib(n):
 
 q = fib(10000)
 
-vingprint(q)
+print(q)
 
-# Thanks to some kids in Singapore
+def foo(x,b):
+    if x > b:
+        print(x)
+    else:
+        print(b)
+        
+x = 1
+b = 2
+
+foo(x,b)
+
+q = np.array(q)
+
+print(type(q))
+
+print(q)
+
+my_dict = {"a": "foo",
+           "b": "bar",
+           "c": [[1,2],
+               [3,4]]}
+
+print(my_dict)
